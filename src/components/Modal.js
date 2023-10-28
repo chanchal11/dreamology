ce36c9b5112e9bc13e5948b15cc9805a58dd31cc
Chanchal Kumar <rrabshrivastava@gmail.com>
2023-10-28 21:06:06 +0530

    show modal

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Image from 'next/image';
+export default function ModalComponent({open, handleClose, dream}) {
+     
+    return (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          className='ex-modal'
+        >
+            <Card>
+            <CardContent>
+            <Image width={300} height={300} src="https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/09/Why-Do-We-Dream-1333685958-770x533-1.jpg" />
+            <Typography variant="h5">{dream.typeOfDream}</Typography>
+            <Typography variant="body1">{dream.prediction}</Typography>
+            <Typography variant="body2">{dream.thingsToDoIfHaveToAvoidIt}</Typography>
+            </CardContent>
+        </Card>
+        </Modal>
+    );
+  };
--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -1,5 +1,7 @@
 import { Typography, Grid, Card, CardContent } from '@mui/material';
 import Image from 'next/image';
+import ModalComponent from '../../components/Modal';
+import { useState } from 'react';
 
 export default function index() {
   const dreams = [
@@ -20,21 +22,27 @@ export default function index() {
     },
   ];
 
+  const [open, setOpen] = useState(false);
+  const [selectedDream, setSelectedDream] = useState({});
   return (
     <div>
       <Typography variant="h1" className='margin-percent' >Explore Dreams</Typography>
-      <Grid  container spacing={2} style={{paddingTop: '50px'}} >
+      <Grid container spacing={2} style={{ paddingTop: '50px' }} >
         {dreams.map((dream, index) => (
           <Grid className='grid-item' width={300} height={500} item xs={12} sm={6} md={4} key={index}>
-            <Card>
+            <Card onClick={() => {
+              setOpen(true);
+              setSelectedDream(dream);
+            }} >
               <CardContent>
                 <Image width={300} height={300} src="https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/09/Why-Do-We-Dream-1333685958-770x533-1.jpg" />
                 <Typography variant="h5">{dream.typeOfDream}</Typography>
-                </CardContent>
+              </CardContent>
             </Card>
           </Grid>
         ))}
       </Grid>
+      <ModalComponent open={open} handleClose={() => setOpen(false)} dream={selectedDream} />
     </div>
   );
 }
